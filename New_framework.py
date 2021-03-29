a0fdb19bd9209b063a5aca3653324ab5cca801c7
NikkiFRTL <70467189+NikkiFRTL@users.noreply.github.com>
2021-03-29 19:14:35 +0500

    Commit #20

--- New_framework.py
+++ New_framework.py
@@ -176,8 +176,8 @@ class Tensor(object):
 a = Tensor([1, 2, 3, 4, 5], autograd=True)
 b = Tensor([2, 2, 2, 2, 2], autograd=True)
 c = Tensor([5, 4, 3, 2, 1], autograd=True)
-d = a * (-b)
-e = (-b) - c
-f = d - e
+d = a * b
+e = b + c
+f = d + e
 f.backward(Tensor(np.array([1, 1, 1, 1, 1])))
 print(b.grad.data == np.array([-2, -2, -2, -2, -2]))
