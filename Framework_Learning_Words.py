a7c1b0fe92422d773f70401a558c5e633b8f3885
NikkiFRTL <70467189+NikkiFRTL@users.noreply.github.com>
2021-04-09 18:26:41 +0500

    Commit #25

--- Framework_Learning_Words.py
+++ Framework_Learning_Words.py
@@ -14,17 +14,19 @@ for line in raw[0:1000]:
 new_tokens = list()
 for line in tokens:
     new_tokens.append(['-'] * (6 - len(line)) + line)
+
 tokens = new_tokens
 
 vocab = set()
 for sent in tokens:
     for word in sent:
         vocab.add(word)
+
 vocab = list(vocab)
 
 word2index = {}
 for index, word in enumerate(vocab):
-    word2index[index] = word
+    word2index[word] = index
 
 
 def word2indices(sentence):
@@ -56,24 +58,60 @@ weight_optimizer = SGD(parameters=params, alpha=0.05)
 
 for iter in range(1000):
 
+    # Число пакетов = 100.
     batch_size = 100
     total_loss = 0
 
+    # Инициализация скрытого слоя рекуррентной сети.
     hidden = model.init_hidden(batch_size=batch_size)
 
+    # Объединение входных данных в пакеты с помощью вложенного цикла for.
+    # В этом наборе данных нет ни одного предложения длиннее (6) шести слов.
+    # Поэтому сеть читает пять первых слов и пыталается предсказать (6) шестое.
     for t in range(5):
-
         input = Tensor(data[0:batch_size, t], autograd=True)
         rnn_input = embed.forward(input=input)
         output, hidden = model.forward(rnn_input, hidden=hidden)
 
+    # Целевое слово.
     target = Tensor(data[0:batch_size, t+1], autograd=True)
+
+    # Прямое распространение.
+    # Мы двигаем вперед пять входных примеров.
     loss = criterion.forward(output, target)
+
+    # Обратное распространение.
+    # Когда происходит вызов loss. backward(),
+    # градиенты распространяются в обратном направлении через всю сеть до входных данных.
     loss.backward()
+
+    # Обучение и корректировака весов.
     weight_optimizer.step()
     total_loss += loss.data
 
-    if iter % 200 == 0:
+    if iter % 100 == 0:
         p_correct = (target.data == np.argmax(output.data, axis=1)).mean()
         print_loss = total_loss / (len(data)/batch_size)
         print(f"Loss: {print_loss}    Correct:  {p_correct}")
+
+# Нейронная сеть, обученная на 100 первых примерах из обучающего набора данных,
+# достигла точности прогнозирования около 37 % (почти идеально для этой задачи).
+# Она предсказывает, куда может выйти Маrу.
+
+# Попробуем предстаказть куда же пошла Mary напустив обученную сеть на пакет из одного предложения.
+batch_size = 1
+hidden = model.init_hidden(batch_size=batch_size)
+for t in range(5):
+    input = Tensor(data[0:batch_size, t], autograd=True)
+    rnn_input = embed.forward(input=input)
+    output, hidden = model.forward(input=rnn_input, hidden=hidden)
+
+target = Tensor(data[0:batch_size, t+1], autograd=True)
+loss = criterion.forward(output, target)
+
+context = ""
+for idx in data[0:batch_size][0][0:-1]:
+    context += vocab[idx] + " "
+print("Context:", context)
+print("True:", vocab[target.data[0]])
+print("Pred:", vocab[output.data.argmax()])
